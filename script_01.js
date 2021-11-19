4f350861c1b4d2f8c498420dcb350cc3e73a6ba5
Christian <chris.moeller-bosse@gmx.de>
2021-11-19 11:35:40 +0100

    /fertig?

--- script_01.js
+++ script_01.js
@@ -1,2 +1,34 @@
 // Entscheidungsstrukturen | control flow
 
+// Deklaration + Assignment
+const ageJohn = 30;
+const ageMark = Number(prompt("Wie alt ist Mark?"));
+
+//Deklaration
+let isJohnOlder;
+
+// Test | Logische Aussage
+
+isJohnOlder = isOlder(ageJohn, ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+
+if (isJohnOlder) {
+    console.log("John ist älter");
+}
+else if (isJohnEqual) {
+    console.log("John ist genauso alt wie Mark")
+}
+else {
+    console.log("John ist jünger");
+}
+console.log(isOlder);
+
+function isOlder(first, second) {
+    return first > second;
+}
